a91767d1f28bd5a024760e57a54dbe802fa4aab8
James Harris <mharris9608@yahoo.com>
2021-01-27 23:37:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,10 +22,11 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each do |season|
-    [:season] << supply
+  holiday_hash.each do |season, holidays|
+    if season == :winter
+      season << supply
+    end
   end
-  holiday_hash
 end
 
 
